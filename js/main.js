2f18d9fdb45b9636324e926a78dd74540a3bb6f5
dedEbalay <lonshakov.max@gmail.com>
2020-10-08 09:40:00 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,36 +1,61 @@
-var hotelSwiper = new Swiper(".swiper-container", {
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false,
-	},
+$(document).ready(function () {
+	var hotelSwiper = new Swiper(".swiper-container", {
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
+		},
 
-	// Optional parameters
-	loop: true,
+		// Optional parameters
+		loop: true,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: ".swiper-button--next",
-		prevEl: ".swiper-button--prev",
-	},
-});
+		// Navigation arrows
+		navigation: {
+			nextEl: ".swiper-button--next",
+			prevEl: ".swiper-button--prev",
+		},
+	});
 
-var rewiesSwiper = new Swiper(".rewies-swiper", {
-	keyboard: {
-		enabled: true,
-	},
-	// Optional parameters
-	loop: true,
+	var rewiesSwiper = new Swiper(".rewies-swiper", {
+		keyboard: {
+			enabled: true,
+		},
+		// Optional parameters
+		loop: true,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: ".rewies-swiper-button__next",
-		prevEl: ".rewies-swiper-button__prev",
-	},
-});
+		// Navigation arrows
+		navigation: {
+			nextEl: ".rewies-swiper-button__next",
+			prevEl: ".rewies-swiper-button__prev",
+		},
+	});
 
-var mobileButton = document.querySelector(".adaptive-button");
-mobileButton.addEventListener("click", function () {
-	document
-		.querySelector(".row--bottom")
-		.classList.toggle("row--bottom__mobile--visible");
+	var mobileButton = document.querySelector(".adaptive-button");
+	mobileButton.addEventListener("click", function () {
+		document
+			.querySelector(".row--bottom")
+			.classList.toggle("row--bottom__mobile--visible");
+	});
+
+	var modalButton = $("[data-toggle=modal]");
+	modalButton.on("click", openModal);
+
+	var closeButton = $(".modal__close");
+	closeButton.on("click", closeModal);
+
+	addEventListener("keyup", function (e) {
+		if (e.keyCode === 27) {
+			var modalClass = $(".modal");
+			modalClass.removeClass("modal--visible");
+		}
+	});
+
+	function openModal() {
+		var modalClass = $(".modal");
+		modalClass.addClass("modal--visible");
+	}
+	function closeModal() {
+		event.preventDefault();
+		var modalClass = $(".modal");
+		modalClass.removeClass("modal--visible");
+	}
 });
